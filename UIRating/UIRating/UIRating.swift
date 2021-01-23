94578a0662b65fbe5cf955f9d609f42fc4d06893
Arman <shadowofarman@hotmail.com>
2021-01-23 15:06:08 +0330

    Added assets

--- UIRating/UIRating/UIRating.swift
+++ UIRating/UIRating/UIRating.swift
@@ -13,6 +13,23 @@ open class UIRating: UIView {
     fileprivate let slider = UISlider()
     fileprivate let stackView = UIStackView()
     
+    /// Sets the amount of option to rate from.
+    open var numberOfOptions: Int = 5
+    /// Sets the spacing between options.
+    open var spacing: CGFloat = 15 {
+        didSet {
+            self.stackView.spacing = self.spacing
+        }
+    }
+    
+    //icons
+    /// Sets the icon to use for an empty rating.
+    lazy open var emptyIcon    = UIImage(named: "UIRating_empty_icon"    , in: Bundle.init(for: type(of: self)), compatibleWith: nil)
+    /// Sets the icon to use for a half rating.
+    lazy open var halfFullIcon = UIImage(named: "UIRating_half_full_icon", in: Bundle.init(for: type(of: self)), compatibleWith: nil)
+    /// Sets the icon to use for a full rating.
+    lazy open var fullIcon     = UIImage(named: "UIRating_full_icon"     , in: Bundle.init(for: type(of: self)), compatibleWith: nil)
+    
 //MARK: View life cycle
     
     override public init(frame: CGRect) {
@@ -28,6 +45,7 @@ open class UIRating: UIView {
 //MARK: - all configs
     fileprivate func allConfigs() {
         self.sliderConfig()
+        self.stackViewConfig()
     }
     
 //MARK: - Slider
@@ -50,6 +68,12 @@ open class UIRating: UIView {
     fileprivate func stackViewConfig() {
         self.addSubview(self.stackView)
         self.stackViewConstraints()
+        self.stackView.isUserInteractionEnabled = false
+        self.stackView.axis = .horizontal
+        self.stackView.alignment = .center
+        self.stackView.distribution = .fillEqually
+        self.stackView.spacing = self.spacing
+        self.optionsConfig()
     }
     //constraints
     fileprivate func stackViewConstraints() {
@@ -59,5 +83,48 @@ open class UIRating: UIView {
         NSLayoutConstraint(item: self.stackView, attribute: .width, relatedBy: .equal, toItem: self.slider, attribute: .width, multiplier: 1, constant: 0).isActive = true
         NSLayoutConstraint(item: self.stackView, attribute: .height, relatedBy: .equal, toItem: self.slider, attribute: .height, multiplier: 1, constant: 0).isActive = true
     }
+    
+//MARK: - icon
+    //config
+    fileprivate func optionsConfig() {
+        for _ in 1...self.numberOfOptions {
+            let option = UIImageView(image: self.emptyIcon)
+            option.contentMode = .scaleAspectFit
+            self.stackView.addArrangedSubview(option)
+        }
+    }
+    
+    
+    //MARK: - Slider objc
+        @objc fileprivate func sliderUpdated(_ slider: UISlider) {
+            
+        }
+        
+        @objc fileprivate func sliderTapped(gestureRecognizer: UIGestureRecognizer) {
+            let pointTapped: CGPoint = gestureRecognizer.location(in: self.slider)
+
+            let positionOfSlider: CGPoint = self.slider.frame.origin
+            let widthOfSlider: CGFloat = self.slider.frame.size.width
+            let newValue = ((pointTapped.x - positionOfSlider.x) * CGFloat(self.slider.maximumValue) / widthOfSlider)
+            self.slider.setValue(Float(newValue), animated: true)
+                if gestureRecognizer.state == .ended {
+                    
+                }
+            self.sliderUpdated(self.slider)
+        }
+        
+        @objc fileprivate func sliderBeingDragged(_ sender: UIScrollView) {
+            
+            self.sliderUpdated(self.slider)
+        }
+        
+        @objc fileprivate func sliderValueChanged(_ sender: UIScrollView) {
+           
+           
+            self.sliderUpdated(self.slider)
+        }
+        
+    }
+
     
 }
